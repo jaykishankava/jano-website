6f51a720250fcf93334a60f5833cb7e0fc577f68
jaykishankava <kava.jaykishan9125@gmail.com>
2024-06-14 10:19:13 +0530

    tailwind-website

---
+++ js/tailwind.config.js
@@ -0,0 +1,20 @@
+ 
+ tailwind.config ={
+  content: [],
+  theme: {
+
+    fontFamily: {
+      'inter': ['Inter', 'sans-serif'],
+      
+    },
+    colors:{
+      'pink' :'#FF3294',
+      'gray' :'#E9E9E9',
+      'white':'#FFFFFF',
+      'black':'#000000',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
